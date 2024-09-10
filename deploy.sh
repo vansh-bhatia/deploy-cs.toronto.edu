3a22c51200ccc4adb18dc41935f7d8cd4b6a3b69
Vansh Bhatia <64160878+vansh-bhatia@users.noreply.github.com>
2024-09-09 22:27:16 -0400

    Added deploy.sh
    added deploy.sh, automate deployment to the cs.toronto.edu server, specifically to the public_html dir

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -p "Enter your username: " SERVER_USER
+
+SERVER_IP="cs.toronto.edu"
+
+BUILD_DIR="./Portfolio" # Update this path based on your directory
+
+
+DEST_DIR="~/public_html" # cs.toronto.edu expects this directory and looks for index.html
+
+echo "Deploying files to the server..."
+scp -r $BUILD_DIR/* $SERVER_USER@$SERVER_IP:$DEST_DIR
+
+if [ $? -eq 0 ]; then
+  echo "Deployment complete!"
+else
+  echo "Deployment failed!"
+fi
