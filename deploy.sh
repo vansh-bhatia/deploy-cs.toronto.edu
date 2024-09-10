4d4a97762e2af043908513b52b186c6f8f890aec
Vansh Bhatia <64160878+vansh-bhatia@users.noreply.github.com>
2024-09-09 23:04:59 -0400

    Update deploy.sh
    comment added

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ SERVER_IP="cs.toronto.edu"
 BUILD_DIR="./Portfolio" # Update this path based on your directory
 
 
-DEST_DIR="~/public_html" # cs.toronto.edu expects this directory and looks for index.html
+DEST_DIR="~/public_html" # cs.toronto.edu expects this directory and looks for index.html if you're deploying your custom website
 
 echo "Deploying files to the server..."
 scp -r $BUILD_DIR/* $SERVER_USER@$SERVER_IP:$DEST_DIR
